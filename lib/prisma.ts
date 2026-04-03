231138067b4de4f8bdaef258186e04ff9e391be0
Khikmatjon <khikmatjonm@gmail.com>
2026-04-04 01:56:41 +0900

    fix: correctly pass pg.Pool to PrismaPg adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const connectionString = process.env.DATABASE_URL!;
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({ connectionString });
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
   return new PrismaClient({ adapter });
 }
 
